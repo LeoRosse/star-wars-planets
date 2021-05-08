ea5eb1a82bc5e5adf95b0e2ae96eba8ddd8e7ae1
Leonardo Rosseti <rosseti@extendi.it>
2021-05-08 14:06:50 +0200

    First commit

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const App: React.FC = () => (
+  <div>
+    <h2>Star Wars Planets</h2>
+    Planets here!!
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const path = require('path');
+
+const deps = require('./package.json').dependencies;
+module.exports = {
+  entry: './src/index',
+  output: {
+    publicPath: 'http://localhost:3002/',
+    path: path.resolve(__dirname, './build'),
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+    alias: {
+      src: path.resolve(__dirname, 'src'),
+    },
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+    port: 3002,
+    stats: 'normal',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'planets',
+      filename: 'remoteEntry.js',
+      remotes: {},
+      exposes: {
+        './Planets': './src/app',
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: deps['react-dom'],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
+};
